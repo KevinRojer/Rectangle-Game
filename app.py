3da60c4d625d485e9714ca359b19d5f8edf2fe7b
Kevin Rojer <kevin.rojer@live.com>
2023-08-14 18:59:34 +0200

    Changed location

--- app.py
+++ app.py
@@ -6,64 +6,12 @@ Created on Mon Jul 31 20:30:27 2023
 @author: kevinrojer
 """
 
-from src.point import Point
-from src.rectangle import Rectangle
 
-
-def initializeMap():
-    
-    while (True):
-        # Display the options
-        print(" "*5 + "Initialize Map" + " "*5)
-        print("-"*25)
-        print()
-        
-        width = input("Please, choose the width: ")
-        try:
-            # Validate the input
-            width = int(width)
-        except ValueError:
-            print("\nInvalid input. Please, try agian.\n")
-            continue
-            
-        height = input("Please, choose the height: ")
-        
-        try:
-            # Validate the input
-            height = int(height)
-        except ValueError:
-            print("\nInvalid input. Please, try again.\n")
-            continue
-            
-        # Initialize the grid
-        gameMap = Rectangle(width, height)
-        
-        x = input("Please, select an origin x-coordinate: ")
-        try:
-            # Validate input
-            xVal = int(x)
-        except ValueError:
-            print("\nInvalid input. Please, try again.\n")
-            continue
-            
-        y = input("Now, select the y-coordinate: ")
-        try:
-            # validate input
-            yVal = int(y)           
-        except ValueError:
-            print("\nInvalid input. Please, try again.\n")
-            continue
-        
-        # Initialize the map
-        gameMap = gameMap.createMap(xVal, yVal)
-        break
-        
-    print("\nMap initialized.\n")
-    
-    return gameMap
+from src.maps.GameMap import GameMap
 
 
 def showMenu():
+    print()
     print(" "*10 + "Menu" + " "*10)
     print("-"*25)
     print("1. Play Game")
@@ -73,8 +21,9 @@ def showMenu():
 
 
 def main():
-    # start default game
-    gameMap = initializeMap()
+    # initialize the game map
+    gameMap = GameMap()
+    gameMap.initializeMap()
     
     while (True):
         showMenu()
@@ -83,14 +32,17 @@ def main():
 
         if (choice == "1"):
             print("\nyou chose number 1.\n")
+            continue
         elif (choice == "2"):
-            print("\nYou chose number 2.\n")
+            gameMap.changeMap()
+            continue
         elif (choice == "3"):
             print("\nExiting game...")
             break
         else:
             print("\nInvalid input. Please, choose a number between 1 and 3.\n")
             continue
+        
         
     # Print a good by message
     print("Thanks for playing. Bye, bye!")
