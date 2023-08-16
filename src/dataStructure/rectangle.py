367b2700ac2dced17f01f95f36a3206bb789107a
Kevin Rojer <kevin.rojer@live.com>
2023-08-16 13:01:09 +0200

    Update rectangle.py
    Refactored the code to validate positive integers before setting the height and width. Designed the code according to the principle of separation of concerns. Hence, to increase modularity and maintainability, the validation of value is now outside of the setValue functions.

--- src/dataStructure/rectangle.py
+++ src/dataStructure/rectangle.py
@@ -13,18 +13,20 @@ class Rectangle:
     def __init__(self, width, height):
         self.width = width
         self.height = height
+        self.lowerLeft = None
+        self.upperRight = None
         
         
     def createMap(self, startX, startY):
         """
-        
+        Create a map using the given coordinates as the lower-left corner
 
         Parameters
         ----------
         startX : int
-            the smallest x-coordinate.
+            the x-coordinate of the lower left corner.
         startY : int
-            the smallest y-coordinate.
+            the y-coordinate of the lower left corner.
 
         Returns
         -------
@@ -35,27 +37,23 @@ class Rectangle:
             self.lowerLeft = (startX, startY)
             self.upperRight = (startX + self.width, startY + self.height)
         else:
-            print("Invalid input for coordinates.")
+            raise ValueError("Invalid input for coordinates.")
             
     
     def setWidth(self, width):
         """
-        
+        Set the width of the rectangle.
 
         Parameters
         ----------
-        width : TYPE
-            DESCRIPTION.
-
-        Returns
-        -------
-        None.
+        width : int
+            The new width of the rectangle.
 
         """
-        if (isinstance(width, int) and width > 0):
+        if self._validate_positive_integer(width):
             self.width = width
         else:
-            print("Invalid input. Please, try again.")
+            raise ValueError("Please, use positive integers for width.")
         
         
     def setHeight(self, height):
@@ -64,27 +62,40 @@ class Rectangle:
 
         Parameters
         ----------
-        height : TYPE
-            DESCRIPTION.
+        height : int
+            The new height of the rectangle.
+
+        """
+        if self._validate_positive_integer(height):
+            self.height = height
+        else:
+            raise ValueError("Please, use positive integers for height.")
+            
+            
+    def _validate_positive_integer(self, value):
+        """
+        Validate whether a given value is a positive integer.
+
+        Parameters
+        ----------
+        value : int
+            The value to be validated.
 
         Returns
         -------
-        None.
+        bool
+            True if the value is a positive integer. False otherwise.
 
         """
-        if (isinstance(height, int) and height > 0):    
-            self.height = height
+        if (isinstance(value, int) and value > 0):
+            return True
         else:
-            print("Invalid input. Please, try again.")
+            return False
         
         
     def getMapSize(self):
         """
-        
-
-        Returns
-        -------
-        None.
+        Print the size of the map.
 
         """
         print(f"Map size is W {self.width} x H {self.height} grid.")
