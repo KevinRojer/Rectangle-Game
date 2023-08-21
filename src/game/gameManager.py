cb16031ec3ed453e5240d299e6ee00daebda71ae
Kevin Rojer <kevin.rojer@live.com>
2023-08-21 19:52:05 +0200

    Update gameManager.py
    Created a function to randomly place a target inside the map. Also, implemented a function to check whether the target has been hit or missed.

--- src/game/gameManager.py
+++ src/game/gameManager.py
@@ -7,6 +7,7 @@ Created on Fri Aug 18 17:12:39 2023
 """
 
 
+import random
 from src.maps.GameMap import GameMap
 from src.tools.utility import validate_positive_integer_input
 from src.dataStructure.point import Point
@@ -19,11 +20,13 @@ class GameManager():
     def __init__(self):
         self.num_pieces = 1
         self.map = GameMap()
+        self.targets = []
         
         
     def initialize_map(self):
 
         self.map.initializeMap()
+        self._place_targets()
         
         
     def update_map(self):
@@ -31,13 +34,35 @@ class GameManager():
         self.map.changeMap()
 
         
-    def check_for_hits(self, point):
+    def _validate_target(self, point):
         
         return self.map.is_point_on_map(point)
     
     
+    def _place_targets(self):
+        
+        for tg in range(self.num_pieces):
+            
+            target_x = random.randint(0, self.map.map.width)
+            target_y = random.randint(0, self.map.map.height)
+            
+            target = Point(target_x, target_y)
+            self.targets.append(target)
+            
+            print(f"x: {target_x} ; y: {target_y}")
+    
+    
     def get_map_dimensions(self):
         self.map.map.getMapSize()
+        
+        
+    def check_for_hits(self, point):
+        
+        for tg in self.targets:
+            if (point.x == tg.x) and (point.y == tg.y):
+                print("Success. Target hit!")
+            else:
+                print("Target missed. Please, try again.")
         
         
     def select_target(self):
@@ -50,8 +75,8 @@ class GameManager():
         
         target = Point(x_coordinate, y_coordinate)
         
-        if (self.check_for_hits(target)):
-            print("Target in map")
+        if (self._validate_target(target)):
+            self.check_for_hits(target)
         else:
             print("Target outside of map")
             
