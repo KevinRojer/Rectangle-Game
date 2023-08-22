757ec30ed8adabc29bac3aa5bfd19160fe47a570
Kevin Rojer <kevin.rojer@live.com>
2023-08-22 12:42:13 +0200

    Update display.py
    Added new display messages.

--- src/tools/display.py
+++ src/tools/display.py
@@ -21,5 +21,29 @@ def show_exit_message():
     print("Thanks for playing. Bye, bye!")
     
     
+def show_closing_application_message():
+    print("\nExiting application...\n")
+    
+    
 def show_invalid_main_menu():
     print("\nInvalid input. Please, choose a number between 1 and 3.\n")
+    
+    
+def prompt_target_input(dimension):
+    return input(f"Please, select your target's {dimension}-coordinate: ")
+    
+    
+def show_invalid_input(error_message):
+    print(f"\nInvalid input: {error_message}\n")
+    
+    
+def show_target_missed():
+    print("\nTarget missed. Please, try again.\n")
+    
+    
+def show_target_hit():
+    print("\nBoom! Target hit!\n")
+    
+    
+def show_input_out_of_map():
+    print("\nInput error: Selected target is outside of the map.\n")
