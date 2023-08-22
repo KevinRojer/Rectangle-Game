ee1e25f8b1c0a94731c316b60b12d6b8c5530fba
Kevin Rojer <kevin.rojer@live.com>
2023-08-22 12:41:46 +0200

    Update point.py
    Removed unnecessary function in the class

--- src/dataStructure/point.py
+++ src/dataStructure/point.py
@@ -6,27 +6,4 @@ class Point:
     def __init__(self, x, y):
         self.x = x
         self.y = y
-        
-        
-    def isPointInRectangle(self, lowerLeft, upperRight):
-        """
-        
-
-        Parameters
-        ----------
-        lowerLeft : TYPE
-            DESCRIPTION.
-        upperRight : TYPE
-            DESCRIPTION.
-
-        Returns
-        -------
-        bool
-            DESCRIPTION.
-
-        """
-        if lowerLeft[0] < self.x < upperRight[0] \
-        and lowerLeft[1] < self.y < upperRight[1]:
-            return True
-        else:
-            return False
+    
