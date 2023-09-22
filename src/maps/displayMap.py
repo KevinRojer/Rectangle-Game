47329aefb221b3e787cac9b3fcd6c41dda63b741
Kevin Rojer <kevin.rojer@live.com>
2023-09-22 19:06:48 +0200

    Create displayMap.py

---
+++ src/maps/displayMap.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 22 19:02:19 2023
+
+@author: kevinrojer
+"""
+
+
+import turtle
+from src.dataStructure.rectangle import Rectangle
+
+class GuiMap(Rectangle):
+    
+    
+    def draw(self, canvas):
+        pass
