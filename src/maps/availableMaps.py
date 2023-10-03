d0e8245f6c0f08357986c5cd1bad518dafd85bed
Kevin Rojer <kevin.rojer@live.com>
2023-10-03 22:39:20 +0200

    Create availableMaps.py

---
+++ src/maps/availableMaps.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct  3 22:23:06 2023
+
+@author: kevinrojer
+"""
+
+
+from src.maps.displayMap import Map
+
+
+MAPS = [
+        Map("Aruba", 10, 10),
+        Map("Bonaire", 5, 5),
+        Map("Curacao", 7, 7),
+        ]
